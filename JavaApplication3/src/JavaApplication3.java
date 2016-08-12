648c8b12a18fa611debc4c09e0e37d10c32a6575
Fede Mendez <Fede Mendez@DESKTOP-LKDP29J>
2016-08-12 14:57:41 -0300

    commit4

---
+++ JavaApplication3/src/JavaApplication3.java
@@ -0,0 +1,24 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author joa_b
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "¡Hola mundo!");
+        // TODO code application logic here
+    }
+    
+}
